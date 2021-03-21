22d50da214f78f5832b157e2cff6705ddc75e09b
joshkokatnur <77566219+joshkokatnur@users.noreply.github.com>
2021-03-21 16:30:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.9",
-        "License :: MIT",
+        "License :: OSI Approved :: MIT License",
     ],
 )
